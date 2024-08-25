c4ab0d63a5fb3bf5e099a20d18d857c1d8c64f45
hridaybasa <hridaybasa461@gmail.com>
2024-08-25 12:40:48 +0530

    newsletter function

--- script.js
+++ script.js
@@ -122,4 +122,74 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
+
+  // newsletter
+  // const scriptURL =
+  //   "https://script.google.com/macros/s/AKfycbwXL5oKO7-Yu8OwQL6XD3-tDqwxWZhZrWXXY7aslkVrGL25bCZP4xVkN05R3dEx3iEazA/exec"; // replace with your Google Apps Script URL
+  // const form = document.forms["newsletter-form"];
+  // const responseDiv = document.getElementById("response");
+
+  // form.addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  //   responseDiv.innerHTML = "Submitting...";
+
+  //   fetch(scriptURL, { method: "POST", body: new FormData(form) })
+  //     .then((response) => response.json())
+  //     .then((response) => {
+  //       if (response.result === "success") {
+  //         responseDiv.innerHTML = "Thank you for subscribing!";
+  //         form.reset();
+  //       } else {
+  //         responseDiv.innerHTML = "There was an error. Please try again.";
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       responseDiv.innerHTML = "There was an error. Please try again.";
+  //       console.error("Error!", error.message);
+  //     });
+  // });
+
+  document
+    .getElementById("newsletter-form")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const form = new FormData(e.target);
+      const responseDiv = document.getElementById("response");
+
+      // Extracting checkbox values manually
+      const types = [];
+      document
+        .querySelectorAll('input[name="type"]:checked')
+        .forEach((checkbox) => {
+          types.push(checkbox.value);
+        });
+
+      // Manually append checkbox data to FormData
+      form.delete("type"); // Remove existing 'type' key to avoid duplication
+      form.append("type", types.join(", "));
+
+      responseDiv.innerHTML = "Submitting...";
+
+      fetch(
+        "https://script.google.com/macros/s/AKfycbwAUxjxBnmh7gE4vwIv35_EIWCsyU2_J7E8-H7ymH41rJhHRzKmTeElzmtVYB7n_waOtg/exec",
+        {
+          method: "POST",
+          body: form,
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.result === "success") {
+            responseDiv.innerHTML = "Thank you for subscribing!";
+            e.target.reset();
+          } else {
+            responseDiv.innerHTML = "There was an error. Please try again.";
+          }
+        })
+        .catch((error) => {
+          responseDiv.innerHTML = "There was an error. Please try again.";
+          console.error("Error!", error.message);
+        });
+    });
 });
